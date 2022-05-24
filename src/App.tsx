0ef60b44fd3ea56d04a6a5691d55692b506ba73f
Omri Wallach <omriwallach1992@gmail.com>
2022-05-24 13:18:27 +0200

    lint and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { Route, Routes } from "react-router-dom"
-import {pagesRoute} from "./pages"
+import { Route, Routes } from 'react-router-dom';
+import { pagesRoute } from './pages';
 
-export const App = () => <Routes>
-  {
-    pagesRoute.map(({ Page, path }) => <Route path={path} key={path} element={<Page />}/>)
-  }
-</Routes>
+export const App = () => (
+  <Routes>
+    {pagesRoute.map(({ Page, path }) => (
+      <Route path={path} key={path} element={<Page />} />
+    ))}
+  </Routes>
+);
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,36 +1,36 @@
-import { FC } from "react";
-import { articlesPageRoute } from "./articles";
-import { Client } from "./Client";
-import { Contact } from "./Contact";
-import { Intro } from "./Intro"
-import { NotFound } from "./NotFound";
-import { WorkFlow } from "./WorkFlow";
+import { FC } from 'react';
+import { articlesPageRoute } from './articles';
+import { Client } from './Client';
+import { Contact } from './Contact';
+import { Intro } from './Intro';
+import { NotFound } from './NotFound';
+import { WorkFlow } from './WorkFlow';
 
 export interface PageRoute {
-    path: string;
-    Page: FC
+  path: string;
+  Page: FC;
 }
 
 export const pagesRoute: PageRoute[] = [
-    {
-        path: "/",
-        Page: Intro
-    },
-    {
-        path: "/contact",
-        Page: Contact
-    },
-    {
-        path: "*",
-        Page: NotFound
-    },
-    {
-        path: "/workflow",
-        Page: WorkFlow
-    },
-    {
-        path: "/client",
-        Page: Client,
-    },
-    ...articlesPageRoute
-]
+  {
+    path: '/',
+    Page: Intro
+  },
+  {
+    path: '/contact',
+    Page: Contact
+  },
+  {
+    path: '*',
+    Page: NotFound
+  },
+  {
+    path: '/workflow',
+    Page: WorkFlow
+  },
+  {
+    path: '/client',
+    Page: Client
+  },
+  ...articlesPageRoute
+];
