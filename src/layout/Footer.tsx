22ce329beb32bfdd94053c65673aea9e5a64aa64
Omri Wallach <omriwallach1992@gmail.com>
2022-05-25 08:07:27 +0200

    styled footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,1 +1,21 @@
-export const Footer = () => <div>Footer</div>;
+import { FC } from 'react';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import styled from '@emotion/styled';
+
+const StyleContainer = styled(Container)(() => ({ margin: 'auto' }));
+
+export const Footer: FC<any> = () => (
+  <StyleContainer>
+    <Typography variant="body1">My sticky footer can be found here.</Typography>
+    <Typography variant="body2" color="text.secondary">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date('2022').getFullYear()}
+      {'.'}
+    </Typography>
+  </StyleContainer>
+);
